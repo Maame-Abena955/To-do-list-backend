d054032d9d239832cc5e5982389666af706f0225
Maame Abena Nyamekye <hedwignkuah@gmail.com>
2025-07-16 12:16:12 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+# Load environment variables from .env file
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Get DATABASE_URL from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# SessionLocal class to create sessions for interacting with the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class to inherit from for ORM models
 Base = declarative_base()
